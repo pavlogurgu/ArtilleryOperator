55d79f88d6ffcd3797d1535d2fb747aff0d5a02b
pavlogurgu <pavlo.gurgu@gmail.com>
2023-03-16 15:51:32 +0200

    project development beginning

---
+++ src/components/ControlButtons/ControlButtons.jsx
@@ -0,0 +1,12 @@
+export const ControlButtons = () => {
+  return (
+    <>
+      <button>Up</button>
+      <button>Left</button>
+      <button>Right</button>
+      <button>Down</button>
+      <button>Fire</button>
+
+    </>
+  );
+};
