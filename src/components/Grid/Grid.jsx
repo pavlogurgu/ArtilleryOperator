743aa46a09f3f3091342e6c7a8356108dc0ead70
pavlogurgu <pavlo.gurgu@gmail.com>
2023-03-16 18:57:16 +0200

    making base

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,19 +1,21 @@
-import GridSquare from './GridSquare'
-export const Grid = () => {
-    const grid = []
-    for (let row = 0; row < 18; row ++) {
-        grid.push([])
-        for (let col = 0; col < 10; col ++) {
-            grid[row].push(<GridSquare key={`${col}${row}`} color="1" />)
-        }
-    }
-  return (
-    <>
-     <div className='grid-board'>
-            {/* {grid}
-             */}
 
-        </div>
-    </>
-    )
-};
+import { cells } from './fieldSquares';
+
+
+
+
+
+export const Grid = () => {
+
+
+    
+    
+    return (   <>
+    <ul>
+    {cells.map((data, id) => (
+            <li key = {id}>{data.xValue}{data.yValue}</li>
+          ))}
+    </ul>
+        
+    </>)
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -8,3 +8,7 @@ export const store = configureStore({
     // filters: filtersReducer,
   },
 });
+
+
+
+
